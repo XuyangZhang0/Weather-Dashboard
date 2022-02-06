782ce1813341d06238a09df8e9ddf7fc3733080a
Xuyang Zhang <zhangxuyang.chn@gmail.com>
2022-02-05 23:10:52 -0500

    Added a utility function to check whether user chose one of the datalist options, and set lat, lon var accordingly

--- assets/script/script.js
+++ assets/script/script.js
@@ -43,6 +43,11 @@
 //Search
 //read the keyword, construct url for fetch
 
+// vairables
+let lat = "";
+let lon = "";
+
+
 // Get Coordinates
 function getGeo(cityName) {
     let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=9423c300333017676b7d7fdebf4d0575`;
@@ -58,7 +63,7 @@ function getGeo(cityName) {
             if (!data) {
                 alert("We ran into some issues, please contact the developer at zhangxuyang.chn@gmail.com");
             } else {
-                console.log(data);
+                // console.log(data);
                 renderCityResults(data);
             }
         })
@@ -70,30 +75,68 @@ function getGeo(cityName) {
 {/* <option value="New York"> */ }
 function renderCityResults(geoCodingJSON) {
     for (let i = 0; i < geoCodingJSON.length; i++) {
-        //     const city = {
-        //         name: geoCodingJSON[i].name,
-        //         state: geoCodingJSON[i].state,
-        //         country: geoCodingJSON[i].country,
-        //         lat: geoCodingJSON[i].lat,
-        //         lon: geoCodingJSON[i].lon
-        //     }
-        // cities.push(city);
+
         $("#citylistOptions").append(
             `<option data-lat="${geoCodingJSON[i].lat}" data-lon="${geoCodingJSON[i].lon}" value="${geoCodingJSON[i].name}, ${geoCodingJSON[i].state}, ${geoCodingJSON[i].country}">`
-                `<option value="${geoCodingJSON[i].name}, ${geoCodingJSON[i].state}, ${geoCodingJSON[i].country}">`
         )
     }
 
+}
+// Event Lisener on input field selected
+$("#citylist").on("input", function (event) {
+    if (event.inputType == "insertReplacementText" || event.inputType == null) {
+        checkExists($('#citylist').val());
+    }
+})
+
+// Utility Function to ensure a datalist item is selected, and variable set
+function checkExists(inputValue) {
+    var x = document.getElementById("citylistOptions");
+    var i;
+    var flag;
+    for (i = 0; i < x.options.length; i++) {
+        if (inputValue == x.options[i].value) {
+            flag = true;
+            lat = x.options[i].dataset.lat;
+            lon = x.options[i].dataset.lon;
+        }
+    }
+    return flag;
 }
 
 // Event Listener bound to keystroke against text field
 let cityInputEl = $("#citylist")[0];
 cityInputEl.addEventListener("keyup", function () {
-    // console.log(cityInputEl.value);
-    // console.log(cityInputEl.value.length);
-
     if (cityInputEl.value.length > 2) {
         $("#citylistOptions").empty();
         getGeo(cityInputEl.value);
     }
 })
+
+let searchBtn = $("#searchBtn")[0];
+searchBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    getWeather(lat, lon);
+})
+
+// Get Weather
+function getWeather(lat, lon) {
+    let requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=9423c300333017676b7d7fdebf4d0575`;
+    fetch(requestUrl)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return;
+            }
+        })
+        .then(function (data) {
+            if (!data) {
+                alert("We ran into some issues, please contact the developer at zhangxuyang.chn@gmail.com");
+            } else {
+                console.log(data);
+
+            }
+        })
+}
+
