94ef526bec1c969017236256b3a4a3802ac4f4e2
Xuyang Zhang <zhangxuyang.chn@gmail.com>
2022-02-06 10:28:34 -0500

    Added localStorage to save recent searches as persistent data, and corresponding array manipulation, button group reload, etc.

--- assets/script/script.js
+++ assets/script/script.js
@@ -46,6 +46,7 @@
 // vairables
 let lat = "";
 let lon = "";
+let recentSearch = [];
 
 
 // Get Coordinates
@@ -72,7 +73,7 @@ function getGeo(cityName) {
 // getGeo("Atlanta");
 
 // Parse geocoding API results
-/* <option value="New York"> */ 
+/* <option value="New York"> */
 function renderCityResults(geoCodingJSON) {
     for (let i = 0; i < geoCodingJSON.length; i++) {
 
@@ -87,7 +88,7 @@ function renderWeatherResults(weatherJSON) {
 
     // weatherJSON.current.weather[0].icon
     $("#currentcardheader").text($('#citylist').val());
-    $("#currentcardheader").append (
+    $("#currentcardheader").append(
         `<div id="icon"><img id="currentwicon" src="" alt=""></div>`
     )
     let iconurl = "http://openweathermap.org/img/w/" + weatherJSON.current.weather[0].icon + ".png";
@@ -109,7 +110,7 @@ function renderWeatherResults(weatherJSON) {
     // data.daily[1].wind_speed
     // data.daily[1].humidity
     // data.daily[1].uvi
-    
+
 }
 // Event Lisener on input field selected
 $("#citylist").on("input", function (event) {
@@ -125,6 +126,7 @@ function checkExists(inputValue) {
     var flag;
     for (i = 0; i < x.options.length; i++) {
         if (inputValue == x.options[i].value) {
+            cityInputEl.removeEventListener("keyup", refreshCityList); //stops input datalist refresh
             flag = true;
             lat = x.options[i].dataset.lat;
             lon = x.options[i].dataset.lon;
@@ -135,17 +137,26 @@ function checkExists(inputValue) {
 
 // Event Listener bound to keystroke against text field
 let cityInputEl = $("#citylist")[0];
-cityInputEl.addEventListener("keyup", function () {
+cityInputEl.addEventListener("keyup", refreshCityList)
+
+function refreshCityList() {
     if (cityInputEl.value.length > 2) {
         $("#citylistOptions").empty();
         getGeo(cityInputEl.value);
     }
-})
+}
 
 let searchBtn = $("#searchBtn")[0];
 searchBtn.addEventListener("click", function (event) {
     event.preventDefault();
     getWeather(lat, lon);
+    let search = {
+        city : $('#citylist').val(),
+        latitude : lat,
+        longitude : lon
+    };
+    saveRecentSearch(search);
+    loadRecentSearch();
 })
 
 // Get Weather
@@ -163,9 +174,29 @@ function getWeather(lat, lon) {
             if (!data) {
                 alert("We ran into some issues, please contact the developer at zhangxuyang.chn@gmail.com");
             } else {
-                console.log(data);
                 renderWeatherResults(data);
             }
         })
 }
 
+// save recent search into array and set localstorage
+function saveRecentSearch (searchInstance) {
+recentSearch.push(searchInstance);
+localStorage.setItem("recentWeatherSearch",JSON.stringify(recentSearch));
+}
+
+// reload the recent search button group
+function loadRecentSearch () {
+    recentSearch = JSON.parse(localStorage.getItem("recentWeatherSearch")) || [];
+    $("#recentSearchBtnGroup").empty();
+    for(let i=0; i<recentSearch.length ; i++) {
+        $("#recentSearchBtnGroup").append(
+            `<button type="button" class="btn btn-secondary my-1 rounded" data-lat="${recentSearch[i].lat}" data-lon="${recentSearch[i].lat}">${recentSearch[i].city}</button>`
+            );
+    }
+
+}
+
+loadRecentSearch();
+
+// Event Listener to the recent search button, with event delegation
